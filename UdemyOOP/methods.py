aaf2775dfe863c02e905ad3e16e5653dc47fea22
Haralambi Manolov <h.manolov77@gmail.com>
2022-05-30 22:10:41 +0300

    Create methods.py

---
+++ UdemyOOP/methods.py
@@ -0,0 +1,32 @@
+class Backpack:
+
+    def __init__(self):
+        self._items = []
+
+    @property
+    def items(self):
+        return self._items
+
+    def add_item(self, item):
+        if isinstance(item, str):
+            self._items.append(item)
+        else:
+            print("Please enter a valid item.")
+
+    def remove_item(self, item):
+        if item in self._items:
+            self._items.remove(item)
+            return 1
+        else:
+            return 0
+
+    def has_item(self, item):
+        return item in self._items
+
+
+backpack1 = Backpack()
+backpack1.add_item('pencil')
+backpack1.add_item('pen')
+backpack1.remove_item('bag')
+print(backpack1.items)
+print(backpack1.has_item('pen'))
