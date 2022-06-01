4c48496a1229010564e8ada17313c73a3433b476
Haralambi Manolov <h.manolov77@gmail.com>
2022-06-01 20:50:14 +0300

    Update

--- UdemyOOP/methods.py
+++ UdemyOOP/methods.py
@@ -23,6 +23,12 @@ class Backpack:
     def has_item(self, item):
         return item in self._items
 
+    def show_items(self, sorted_=False):
+        if sorted_:
+            print(sorted(self._items))
+        else:
+            print(self._items)
+
 
 backpack1 = Backpack()
 backpack1.add_item('pencil')
@@ -30,3 +36,7 @@ backpack1.add_item('pen')
 backpack1.remove_item('bag')
 print(backpack1.items)
 print(backpack1.has_item('pen'))
+
+backpack1.add_item('water bottle')
+
+backpack1.show_items(True)
