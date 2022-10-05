3bb6ab42f18c025c1f7910e0003157303b29f968
Harry <h.manolov77@gmail.com>
2022-10-05 10:52:18 +0300

    Add final functionalities

--- OOP/OOP October 22/dice_game.py
+++ OOP/OOP October 22/dice_game.py
@@ -44,5 +44,51 @@ class Player:
     def roll_die(self):
         return self._die.roll()
 
+
 class DiceGame:
-    pass
+    
+    def __init__(self, player, computer):
+        self._player = player
+        self._computer = computer
+
+    def play(self):
+        print("=============================================")
+        print("Welcome to the newest version of Dice Game!")
+        print("=============================================")
+        while True:
+            self._play_round()
+            # break out of the loop if any player gets to 0(he wins)
+            if self._computer.counter == 0:
+                print("The computer won the game.")
+                break
+            elif self._player.counter == 0:
+                print("You won the game. Congratulations!")
+                break
+
+    def _play_round(self):
+        print("\n\t\tNew Round!\t\t")
+        input("Press Enter or any key to continue...\n")
+
+        player_value = self._player.roll_die()
+        computer_value = self._computer.roll_die()
+
+        print(f"Your die: {player_value}")
+        print(f"Computer die: {computer_value}")
+
+        if player_value > computer_value:
+            self._player.decrement_counter()
+            self._computer.increment_counter()
+            print("You won this round!")
+        elif computer_value > player_value:
+            self._computer.decrement_counter()
+            self._player.increment_counter()
+            print("The computer won this round.")
+        else:
+            print("It's a Tie!")
+
+        print(f"Your counter: {self._player.counter}")
+        print(f"Computer's counter: {self._computer.counter}")
+
+
+game = DiceGame(Player(Die(), is_computer=False), Player(Die(), is_computer=True))
+game.play()
